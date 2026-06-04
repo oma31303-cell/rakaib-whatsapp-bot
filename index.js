fe9b823a0f1f928dfcb0d0e0af04feaef3e5763e
oma31303-cell <oma313.03@gmail.com>
2026-06-05 02:31:32 +0300

    Update index.js

--- index.js
+++ index.js
@@ -2,15 +2,34 @@ const { Client, LocalAuth } = require('whatsapp-web.js');
 const qrcode = require('qrcode-terminal');
 
 const client = new Client({
-    authStrategy: new LocalAuth(),
+    authStrategy: new LocalAuth({
+        dataPath: './.wwebjs_auth'
+    }),
     puppeteer: {
-        executablePath: '/usr/bin/google-chrome-stable',
         headless: true,
-        args: ['--no-sandbox', '--disable-setuid-sandbox']
+        args: [
+            '--no-sandbox',
+            '--disable-setuid-sandbox',
+            '--disable-dev-shm-usage',
+            '--disable-accelerated-2d-canvas',
+            '--no-first-run',
+            '--no-zygote',
+            '--single-process',
+            '--disable-gpu'
+        ]
     }
 });
 
-client.on('qr', (qr) => { qrcode.generate(qr, { small: true }); });
-client.on('ready', () => { console.log('البوت صاحي يا مولاي!'); });
+client.on('qr', (qr) => {
+    qrcode.generate(qr, { small: true });
+});
+
+client.on('ready', () => {
+    console.log('البوت صاحي يا مولاي!');
+});
+
+client.on('auth_failure', msg => {
+    console.error('فشل في التوثيق يا مولاي:', msg);
+});
 
 client.initialize();
